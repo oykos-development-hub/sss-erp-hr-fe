0f3556fe15ad24d015bff232aca5f3c38fc1172e
oykos-development-hub <hub@oykos.me>
2023-07-12 12:59:19 +0000

    Update code from GitLab

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import {MicroserviceProps} from './types/micro-service-props';
 import {Router} from './router';
 
 const renderMicroService = (App: React.FC) => {
+  console.log('** renderMicroService triggered **');
   const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
   let container: Root | null = null;
   const renderSlug = 'render' + MICRO_SERVICE_SLUG;
@@ -13,6 +14,7 @@ const renderMicroService = (App: React.FC) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   window[renderSlug] = (containerId: string, props: MicroserviceProps) => {
+    console.log('** renderMicroService render triggered **');
     if (!container) {
       container = ReactDOM.createRoot(document.getElementById(containerId) as HTMLElement);
     }
@@ -23,18 +25,21 @@ const renderMicroService = (App: React.FC) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   window[unmountSlug] = () => {
+    console.log('** renderMicroService unmount triggered **');
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     container?.unmount();
     container = null;
   };
 
+  console.log('** renderMicroService container **', document.getElementById(containerSlug));
   if (!document.getElementById(containerSlug)) {
     root.render(<App />);
   }
 };
 
 renderMicroService((props: MicroserviceProps) => {
+  console.log('renderMicroService is about to be triggered...');
   return (
     <React.StrictMode>
       <Router {...props} />
