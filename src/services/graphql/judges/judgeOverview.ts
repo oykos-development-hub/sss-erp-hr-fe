0ab5882d71d7292c5421cd25ddea92e09268dea4
oykos-development-hub <hub@oykos.me>
2023-07-31 11:42:09 +0000

    Update code from GitLab

--- src/services/graphql/judges/judgeOverview.ts
+++ src/services/graphql/judges/judgeOverview.ts
@@ -6,43 +6,43 @@ const judgeOverview = async (
 ): Promise<JudgesOverviewResponse['data']['judges_Overview']> => {
   const response = await GraphQL.fetch(`   
       query {
-        judges_Overview(search: "${data.search || ''}", user_profile_id: ${
-    data.user_profile_id?.id || 0
-  }, organization_unit_id: ${data.organization_unit_id?.id || 0}, page: ${data.page}, size: ${data.size}) {
+        judges_Overview(user_profile_id: ${data.user_profile_id?.id || 0}, organization_unit_id: ${
+    data.organization_unit_id?.id || 0
+  }, page: ${data.page}, size: ${data.size}) {
             message
             status
             total
             items {
-                id
-                organization_unit {
-                    title
-                    id
-                }
-                job_position {
-                    title
-                    id
-                }
-                is_judge_president
-                first_name
-                last_name
-                evaluation
-                created_at
-                updated_at
-                folder_id
-                norms {
-                    id
-                    user_profile_id
-                    area
-                    norm
-                    percentage_of_norm_decrease
-                    number_of_items
-                    number_of_solved_items
-                    start_date
-                    end_date
-                    evaluation
-                    evaluation_valid_to
-                    relocation
-                }
+              id
+              organization_unit {
+                  title
+                  id
+              }
+              job_position {
+                  title
+                  id
+              }
+              is_judge_president
+              first_name
+              last_name
+              created_at
+              updated_at
+              folder_id
+              norms {
+                  id
+                  user_profile_id
+                  topic
+                  title
+                  percentage_of_norm_decrease
+                  number_of_norm_decrease
+                  number_of_items
+                  number_of_items_solved
+                  evaluation_id
+                  date_of_evaluation
+                  date_of_evaluation_validity
+                  file_id
+                  relocation_id
+              }
             }
         }
       }
