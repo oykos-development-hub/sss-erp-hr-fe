20330a8926b021bdd93309208f944a1880eba5bf
oykos-development-hub <hub@oykos.me>
2023-08-25 13:44:39 +0000

    Update code from GitLab

--- src/router.tsx
+++ src/router.tsx
@@ -14,8 +14,6 @@ import ApplicationDetailsScreen from './screens/jobTenders/applicationDetails/ap
 import ApplicationsScreen from './screens/jobTenders/applications/applications';
 import {JobTenderDetailsScreen} from './screens/jobTenders/jobTenderDetails/jobTenderDetails';
 import {JudgesNumbersDetails} from './screens/judges/judgesDecisionDetails/judgesDecisionDetails';
-import Judges from './screens/judges/landing';
-import JobTenders from './screens/jobTenders/landing';
 
 const employeesRegex = /^\/hr\/employees/;
 const systematizationDetailsRegex = /^\/hr\/systematization\/systematization-details(?:\/(\d+))?$/;
@@ -32,7 +30,7 @@ export const Router: React.FC<MicroserviceProps> = props => {
   });
 
   const renderScreen = () => {
-    if (pathname === '/hr') return <LandingScreen context={context} />;
+    if (pathname === '/hr') return context.navigation.navigate('employees');
     if (pathname === '/hr/organization-units') return <OrganizationUnitsScreen context={context} />;
     if (employeesRegex.test(pathname)) return <EmployeesScreen context={context} />;
     if (pathname === '/hr/systematization') return <SystematizationScreen context={context} />;
@@ -43,13 +41,13 @@ export const Router: React.FC<MicroserviceProps> = props => {
     if (ApplicationsRegex.test(pathname)) return <ApplicationsScreen context={context} />;
     if (ApplicationsDetailsRegex.test(pathname)) return <ApplicationDetailsScreen context={context} />;
     if (systematizationDetailsRegex.test(pathname)) return <SystematizationDetails context={context} />;
-    if (pathname === '/hr/judges') return <Judges />;
+    if (pathname === '/hr/judges') return context.navigation.navigate('judges/number-decision');
     if (pathname === '/hr/judges/number-decision') return <JudgesNumberDecisions context={context} />;
     if (pathname === '/hr/judges/number-decision/new-decision')
       return <JudgesNumbersDetails context={context} isNew={true} />;
     if (judgesNumberDetailsRegex.test(pathname)) return <JudgesNumbersDetails context={context} />;
     if (pathname === '/hr/judges/overview-judges-presidents') return <JudgeNorms context={context} />;
-    if (pathname === '/hr/job-tenders') return <JobTenders />;
+    if (pathname === '/hr/job-tenders') return context.navigation.navigate('job-tenders/job-tenders-list');
 
     return <NotFound404 context={context} />;
   };
