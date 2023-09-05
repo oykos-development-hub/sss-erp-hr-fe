3ef1cf0c9c238fa372160025b9ed5241728f0017
oykos-development-hub <hub@oykos.me>
2023-09-05 12:52:22 +0000

    Update code from GitLab

--- src/screens/employees/addNew/addNew.tsx
+++ src/screens/employees/addNew/addNew.tsx
@@ -3,12 +3,14 @@ import {MainTitle, OverviewBox} from '../../../components/employeesList/styles';
 import {ScreenWrapper} from '../../../shared/screenWrapper';
 import {ScreenProps} from '../../../types/screen-props';
 import {BasicInfo} from '../basicInfo/basicInfo';
+import {Divider} from 'client-library';
 
 export const AddNewEmployee: React.FC<ScreenProps> = ({context}: {context: any}) => {
   return (
     <ScreenWrapper context={context}>
       <OverviewBox>
         <MainTitle content="DODAVANJE NOVOG ZAPOSLENOG" />
+        <Divider />
         <BasicInfo context={context} />
       </OverviewBox>
     </ScreenWrapper>
